3f2385d2730731547acb28bc714c0a4ca2502c3f
Arjun Gulyani <arjungulzzz@gmail.com>
2016-08-20 16:29:59 +0530

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,9 +1,10 @@
 def get_next_target(source):
     start_link = source.find('<a href=')
+    if start_link == -1:
+        return None,0
     start_quote = source.find('"',start_link)
     end_quote = source.find('"',start_quote+1)
     url = source[start_quote+1:end_quote]
     return url,end_quote
-    
-    
+url,endpos=get_next_target()
     
