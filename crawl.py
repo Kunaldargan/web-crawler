fc3ce5e1135894ca90d92e14a28ddf23b97f48d4
Arjun Gulyani <arjungulzzz@gmail.com>
2016-08-23 20:41:28 +0530

    add_page_to_index added

--- crawl.py
+++ crawl.py
@@ -57,3 +57,8 @@ def lookup(index,keyword):
         if entry[0]==keyword:
             return entry[1]
     return []
+
+def add_page_to_index(index,url,content):
+    words=content.split()
+    for word in words:
+        add_to_index(index,word,url)
