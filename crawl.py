24f29e42b89b93cc799ae28be6256b0c315de932
Arjun Gulyani <arjungulzzz@gmail.com>
2016-08-23 20:14:55 +0530

    add_to_index added

--- crawl.py
+++ crawl.py
@@ -45,3 +45,9 @@ def crawl_web(seed):
             crawled.append(page)
     return crawled
             
+def add_to_index(index,keyword,url):
+    for entry in index:
+        if entry[0]==keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword,[url]])
